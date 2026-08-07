c0b8b07a7cbc44684e3103f308527506029ab0cb
Abdallah Atef Hatem <abdallahabdallahatef7@gmail.com>
2026-08-07 07:01:05 +0300

    log and utils and exception

---
+++ demo.py
@@ -0,0 +1,15 @@
+from us_visa.logger import logging
+
+# logging.info("Welcome to custom log")
+
+import sys
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+
+try:
+    
+    a = 1 / 0
+except Exception as e:
+    
+    logging.error("Error occurred during computation")
+    raise USvisaException(e, sys)
